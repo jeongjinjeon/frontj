10959eb6fe24ce943724db1be918cf7ec8d242c5
전정진 SKC&C/DevOps <jjjsins@partner.sk.com>
2022-01-12 14:26:37 +0900

    .

--- src/Components/LandingPage/LandingPage.js
+++ src/Components/LandingPage/LandingPage.js
@@ -7,7 +7,7 @@ function LandingPage() {
    
     useEffect(() => {
 
-        axios.get('/api/hello')
+        axios.get('/jjj/api/hello')
         .then(response=>setGreeting(response.data))
         .catch(error => console.log(error))
         
@@ -17,7 +17,7 @@ function LandingPage() {
         <div>
             여기는 리액트 페이지입니다
             {Greeting}  은 스프링에서 가져왔습니다
-
+test
         </div>
     )
 }
